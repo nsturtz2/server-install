20d71567534d27943b9498f69280c3e53e48472a
nsturtz2 <no.reply.nate.sturtz@gmail.com>
2020-05-20 11:43:04 -0500

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -5,9 +5,9 @@ cd /NSCS
 mkdir install
 cd install
 wget https://raw.githubusercontent.com/nsturtz2/server-install/master/install2.sh
-wget https://raw.githubusercontent.com/nsturtz2/server-install/master/install3.sh
 chmod 777 *
 cd /NSCS
 mkdir cron
 cd cron
 mkdir cron.d cron.m cron.w cron.h
+/NSCS/install/install2.sh
