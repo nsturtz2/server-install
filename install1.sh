878f0ac20639d00ebf3effc56408a61f1112719c
nsturtz2 <no.reply.nate.sturtz@gmail.com>
2020-05-24 15:52:32 -0500

    Updated, moved everything to one file,

--- install1.sh
+++ install1.sh
@@ -2,12 +2,29 @@
 cd /
 mkdir /NSCS
 cd /NSCS
-mkdir install
-cd install
-wget https://raw.githubusercontent.com/nsturtz2/server-install/master/install2.sh
-chmod 777 *
-cd /NSCS
 mkdir cron
 cd cron
 mkdir cron.d cron.m cron.w cron.h
-/NSCS/install/install2.sh
+apt install apache2 mysql-server mysql-client bind9 php tree curl unzip zip 
+cd /var/www/
+mkdir search social 
+cd search
+wget https://www.sphider.worldspaceflight.com/download/sphider-3.4.0-MB.zip
+unzip sphider-3.4.0-MB.zip
+cd /var/www/social
+wget https://github.com/humhub/humhub/archive/master.zip
+unzip *
+cd humhub-master
+cp -r * ..
+rm -r *.zip humhub-master
+echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+cd /NSCS
+wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add
+sudo apt update 
+sudo apt install webmin 
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo apt-get update
+sudo apt-get install certbot python3-certbot-apache
+
